614413f60f75488108dd8943fcfb4ac7bb921a3b
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-17 18:17:53 +0600

    form validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a8909732527e5ceec78bd4c1b1df99f3'
 
 posts = [
     {
@@ -30,9 +32,29 @@ def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
-def about_page():
+def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for  {form.username.data}!', 'success')
+        return redirect(url_for("home"))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for("home"))
+        else:
+            flash(f'Login unsuccessful . Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
